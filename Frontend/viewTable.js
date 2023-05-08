1ddbf9890a53a2920b1979e1f1cd85fc1cb27371
LokeshSingh1102 <lokeshsinghbora1102@gmail.com>
2023-05-08 21:01:00 +0530

    writing code for view table

---
+++ Frontend/viewTable.js
@@ -0,0 +1,22 @@
+console.log("running");
+let semester = 6
+let section = 'B'
+let department = BCA
+
+let matrix = [
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""]
+]
+
+const day = ["monday","tuesday","wednesday","thursday","friday","saturday"];
+
+$(document).ready(function(){
+    console.log("ready");
+    
+    
+
+})
