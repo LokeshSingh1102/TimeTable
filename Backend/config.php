5ac6bcd95e72d945f399284c7f636228942356fd
Suvojeet Haldar <suvojeethaldar4@gmail.com>
2023-05-14 15:46:57 +0530

    corrected time, added some features to be used in future

--- Backend/config.php
+++ Backend/config.php
@@ -1,15 +1,31 @@
 <?php
+session_start();
+//Get Server IP
+$ip=$_SERVER['REMOTE_ADDR'];
 $servername = "localhost";
 $username = "root";
 $password = "";
 $database =  "routine maker";
+date_default_timezone_set("Asia/Kolkata");
+$dttm=date('Y-m-d H:i:s');
 
 // Creating a connection
 $conn = mysqli_connect($servername, $username, $password, $database);
-// Die if connection was not successfull
-// if(!$conn){
-//     die("Sorry we failed to connect: ". mysqli_connect_error());
-// }else{
-//     echo '<script>alert("Connection was successful");</script>';
-// }
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  //echo "Connected successfully<br>";
+  //echo '<pre>';
+  //print_r($conn);
+  
+  function conv_date($datetime)
+  {
+      $ret='';
+      if($datetime){
+          $ret=date('d M Y h:i:s A',strtotime($datetime));
+      }
+      return $ret;
+  }
+  
 ?>
