864c5dddb3f1bdd6e5650281c98aa6f823eafaa6
LokeshSingh1102 <lokeshsinghbora1102@gmail.com>
2023-05-24 22:37:08 +0530

    changed code here n there

--- Frontend/Students/viewData.js
+++ Frontend/Students/viewData.js
@@ -1,21 +1,25 @@
 // console.log(cell[4+1-1]);
+let h1 = document.querySelector('h1');
+h1.innerHTML = `Routine ${sessionStorage.getItem("studentDepartment")} ${sessionStorage.getItem("studentSemester")} ${sessionStorage.getItem("studentSection")}`;
+
 const day = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 day.forEach((element, index) => {
     $.ajax({
         type: 'POST',
         url: 'http://localhost/TimeTable/Backend/viewFetchData.php',
         data: {
-            Dept: sessionStorage.getItem("Department"),
-            Semester: sessionStorage.getItem("Semester"),
-            Section: sessionStorage.getItem("Section"),
+            Dept: sessionStorage.getItem("studentDepartment"),
+            Semester: sessionStorage.getItem("studentSemester"),
+            Section: sessionStorage.getItem("studentSection"),
             Day: element
         },
         success: function (data) {
+            console.log(data);
             let i = 0;
             const cell = Array.from(document.getElementsByClassName(`Tdata${index}`));
             while (i < data.length) {
                 var period = data[i].Period;
-                cell[period - 1].innerHTML = data[i].Subject;
+                cell[period - 1].innerHTML = data[i].Subject + " ( "+data[i].Teacher+" )";
                 i = i + 1;
             }
         }
--- Frontend/viewData.js
+++ Frontend/viewData.js
@@ -1,22 +1,24 @@
 let h1 = document.querySelector('h1');
-h1.innerHTML = `Routine ${sessionStorage.getItem("Department")} ${sessionStorage.getItem("Semester")} ${sessionStorage.getItem("Section")}`;
+h1.innerHTML = `Routine ${sessionStorage.getItem("teacherDepartment")} ${sessionStorage.getItem("teacherSemester")} ${sessionStorage.getItem("teacherSection")}`;
+
 const day = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 day.forEach((element, index) => {
     $.ajax({
         type: 'POST',
         url: 'http://localhost/TimeTable/Backend/viewFetchData.php',
         data: {
-            Dept: sessionStorage.getItem("Department"),
-            Semester: sessionStorage.getItem("Semester"),
-            Section: sessionStorage.getItem("Section"),
+            Dept: sessionStorage.getItem("teacherDepartment"),
+            Semester: sessionStorage.getItem("teacherSemester"),
+            Section: sessionStorage.getItem("teacherSection"),
             Day: element
         },
         success: function (data) {
+            console.log(data);
             let i = 0;
             const cell = Array.from(document.getElementsByClassName(`Tdata${index}`));
             while (i < data.length) {
                 var period = data[i].Period;
-                cell[period - 1].innerHTML = data[i].Subject;
+                cell[period - 1].innerHTML = data[i].Subject + " ( "+data[i].Teacher+" )";
                 i = i + 1;
             }
         }
